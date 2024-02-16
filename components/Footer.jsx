1db0e25f477589e9699172bc9ee253d63e028d04
Antarcticpx <antarcticpx@gmail.com>
2024-02-16 20:59:20 +0700

    Add Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 py-4 min-w-full mt-20">
+    <footer className="bg-gray-900 py-4 min-w-full mt-20">
         <p className='text-center text-white'>&copy; Copyright 2024. All Right Reserved</p>
     </footer>
   )
