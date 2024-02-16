391936d5b4314a8bea2354425ad52644ff321a9a
Antarcticpx <antarcticpx@gmail.com>
2024-02-16 20:59:06 +0700

    Add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,6 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 py-4 min-w-full mt-20">
         <p className='text-center text-white'>&copy; Copyright 2024. All Right Reserved</p>
-      
     </footer>
   )
 }
